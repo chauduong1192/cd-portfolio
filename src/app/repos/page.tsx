84846f61e4844f5a14bd65b8eeb7b756eaddc8ac
Chau Duong <chau.duong1192@gmail.com>
2024-10-14 15:13:00 +0700

    fix: 🐛 add port

--- src/app/repos/page.tsx
+++ src/app/repos/page.tsx
@@ -11,7 +11,9 @@ export const metadata = {
 };
 
 async function getPosts() {
-  const res = await fetch(`${process.env.API_URL}/api/repos`);
+  const res = await fetch(
+    `${process.env.API_URL ?? 'http://localhost:3000'}/api/repos`,
+  );
 
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
